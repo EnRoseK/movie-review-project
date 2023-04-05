d232d15e483a99eb748a7a2cfd5366bd471a2be8
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-04-05 13:14:55 +0800

    Class

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,15 @@
-import express, { Express } from "express";
-import userRoutes from "./routes/userRoutes";
+import express, { Express } from 'express';
+import cors from 'cors';
+
+import userRoutes from './routes/userRoutes';
+import moviesRoutes from './routes/movieRoutes';
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use("/users", userRoutes);
+app.use('/users', userRoutes);
+app.use('/api/movies', moviesRoutes);
 
 export default app;
---
+++ backend/routes/movieRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { countAllMovies, findAllMovies, findMovieById } from '../controllers/movieController';
+
+const moviesRouter = Router();
+
+moviesRouter.get('/', findAllMovies);
+moviesRouter.get('/count', countAllMovies);
+moviesRouter.get('/:_id', findMovieById);
+
+export default moviesRouter;
